478c5207bb825fb69e5bf8d1cebf3e06b4c7c1d6
bridgerrholt <bridgerrholt@github.com>
2021-06-03 11:19:07 -0600

    Update stringify_to_header to create new directory if doesn't exist

--- src/stringify_to_header/stringify_to_header.cpp
+++ src/stringify_to_header/stringify_to_header.cpp
@@ -2,6 +2,8 @@
 // Created by bridg on 3/5/2021.
 //
 
+#include <filesystem>
+
 #include <stringify_to_header.h>
 
 #include <file_util/load_file_to_string.h>
@@ -10,7 +12,12 @@ void stringifyToHeader(
   std::string const & varNamespace, std::string const & varName,
   std::string const & inputFilename, std::string const & outputFilename)
 {
+  using namespace std::filesystem;
+
   std::ifstream inputFile {inputFilename};
+
+  create_directory(path{outputFilename}.parent_path());
   std::ofstream outputFile {outputFilename};
+
   stringifyToHeaderStreamed(varNamespace, varName, inputFile, outputFile);
 }
